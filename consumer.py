6914c3c238bd516ef7da5933c300b4d262fe55c3
Africa-t0t0 <geoorgex@gmail.com>
2021-11-08 21:34:02 -0300

    0.2

--- consumer.py
+++ consumer.py
@@ -7,3 +7,4 @@ for message in consumer:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value))
+    
